1d21829eea39474872bbd1798b70a2b50ff62082
Kevin Eberle <keberle@suse.de>
2015-05-29 14:18:09 +0200

    Changing some instance variables into local ones
    - @vagrant => vagrant
    - @vagrant_libvirt => vagrant_libvirt

--- lib/pennyworth/commands/setup_command.rb
+++ lib/pennyworth/commands/setup_command.rb
@@ -63,19 +63,19 @@ module Pennyworth
     end
 
     def vagrant_installed?
-      @vagrant = Cheetah.run "rpm", "-q", "vagrant", stdout: :capture
-      @vagrant_version = @vagrant.lines.select { |plugin| plugin.start_with?("vagrant") }
+      vagrant = Cheetah.run "rpm", "-q", "vagrant", stdout: :capture
+      @vagrant_version = vagrant.lines.select { |plugin| plugin.start_with?("vagrant") }
 
-      !@vagrant.match(/vagrant-[1-]\.[7-]\.[2-]/).nil?
+      !vagrant.match(/vagrant-[1-]\.[7-]\.[2-]/).nil?
     end
 
     def vagrant_libvirt_installed?
-      @vagrant_libvirt = Cheetah.run "vagrant", "plugin", "list", stdout: :capture
-      @vagrant_libvirt_version = @vagrant_libvirt.lines.select { |plugin|
+      vagrant_libvirt = Cheetah.run "vagrant", "plugin", "list", stdout: :capture
+      @vagrant_libvirt_version = vagrant_libvirt.lines.select { |plugin|
         plugin.start_with?("vagrant-libvirt")
       }
 
-      !@vagrant_libvirt.match(/vagrant-libvirt \(\d\.\d\.29|[3-9]\d\)/).nil?
+      !vagrant_libvirt.match(/vagrant-libvirt \(\d\.\d\.29|[3-9]\d\)/).nil?
     end
 
     private
